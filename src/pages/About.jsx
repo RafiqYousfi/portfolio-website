6d56f9c3f22a883023e8736582d3546c383e1656
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-11-12 15:20:26 +0100

    .

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,25 @@
+import profile from "/public/images/ik.jpg";
+
 const About = () => {
-    return ( <h1>About</h1>
-     );
-}
- 
+  return (
+    <>
+      <img src={profile} alt="Profile" className="about-img" />
+
+      <h1 className="abouth1">
+        Who am <span className="abouth12">I</span>?
+      </h1>
+      <p className="aboutp">
+        Hi, I'm Rafiq — a motivated and curious student who loves to learn and
+        explore new things. I'm passionate about both sports and technology —
+        whether I'm on the football field working with my team or behind the
+        screen writing code, I'm always striving to improve and challenge
+        myself. I enjoy building creative projects, solving problems through
+        coding, and learning how technology can make a real impact. My goal is
+        to keep growing my skills, stay curious, and combine my interests in
+        sports and tech to create something meaningful.
+      </p>
+    </>
+  );
+};
+
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,38 @@
+import React from "react";
+import profile from "/public/images/ik.jpg";
+import laptop from "/public/images/laptop.png"; 
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
-    return ( <h1>Home</h1> );
-}
- 
-export default Home; 
+    const navigate = useNavigate();
+    const goToContact =() => {
+        navigate('/contact')
+    }
+
+  return (
+    <div className="home">
+      <div className="content">
+        <div className="left">
+          <img src={profile} alt="Profile" className="profile-img" />
+        </div>
+
+        <div className="right">
+          <h2 className="name">Rafiq Yousfi</h2>
+          <h1 className="title">
+            Software <br /> Developer
+          </h1>
+
+          <button onClick={goToContact} className="contact-btn">CONTACT ME!</button>
+        </div>
+
+        <div className="laptop">
+          <img src={laptop} alt="Laptop" className="laptop-img" />
+        </div>
+      </div>
+
+      <div className="scroll-down">↓</div>
+    </div>
+  );
+};
+
+export default Home;
