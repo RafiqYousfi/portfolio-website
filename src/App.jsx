ed8d9454dce15bef6c0b1b46ae335ec8b00a468b
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-11-19 16:53:09 +0100

    ,

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/project" element={<Project />} />
+          <Route path="/projects" element={<Project />} />
           <Route path="*" element={<NoPage />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
