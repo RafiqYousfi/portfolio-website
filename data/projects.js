54b0528c247e1ecd84026f973c29f1b8efd1a809
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-11-20 10:58:56 +0100

    .

---
+++ data/projects.js
@@ -0,0 +1,11 @@
+const projects = [
+{
+    id: 1,
+    title: 'Escape Room Game'
+    description: 'An interactive escape room made with Javascript, PHP, HTML and CSS'
+    Image: ;
+
+}
+];
+ 
+export default projects;
---
+++ src/pages/Projectsdetail.jsx
@@ -0,0 +1,5 @@
+const detail = () => {
+    return ( <h1></h1> );
+}
+
+export default detail;
