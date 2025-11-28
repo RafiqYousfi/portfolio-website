95a251d5e061a64c3a16f7ee0737e88969c38d4d
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2025-11-28 13:11:07 +0100

    .

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,19 @@
 const projects = [
-{
-    id: 1,
-    title: 'Escape Room Game',
-    description: 'An interactive escape room made with Javascript, PHP, HTML and CSS',
-    image: '/images/p1foto.jpg',
-    github: 'https://github.com'
+    {
+        id: 1,
+        title: 'Escape Room Game',
+        description: '  Step into a fully interactive browser-based escape room built with PHP, JavaScript, HTML, and CSS. This game challenges players to solve puzzles, uncover hidden clues, and progress through a series of locked environments—all within a seamless, responsive web interface. Every interaction feels dynamic thanks to JavaScript- driven logic, while PHP manages game states and backend processing.',
+image: '/images/p1foto.jpg',
+    github: 'https://github.com/RafiqYousfi/EscaperoomA'
 },
 
 {
     id: 2,
-    title: 'python calculator',
-    description: 'An interactive escape room made with Javascript, PHP, HTML and CSS',
-    image: '/images/Calculator.jpg'
+        title: 'python calculator',
+            description: 'An interactive escape room made with Javascript, PHP, HTML and CSS',
+                image: '/images/Calculator.jpg',
+        github: 'https://github.com/RafiqYousfi/Python-j1'
 }
 ];
- 
+
 export default projects;
