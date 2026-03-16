c78fc89831141be2515eaaf90bfd9da5b3e9724d
9026550@student.zadkine.nl <Ikwilkimpjes@gmail.com>
2026-03-16 14:44:57 +0100

    ?

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,15 +3,15 @@ const projects = [
         id: 1,
         title: 'Escape Room Game',
         description: '  Step into a fully interactive browser-based escape room built with PHP, JavaScript, HTML, and CSS. This game challenges players to solve puzzles, uncover hidden clues, and progress through a series of locked environments—all within a seamless, responsive web interface. Every interaction feels dynamic thanks to JavaScript- driven logic, while PHP manages game states and backend processing.',
-image: '/images/p1foto.jpg',
+image: '../public/images/p1foto.jpg',
     github: 'https://github.com/RafiqYousfi/EscaperoomA'
 },
 
 {
     id: 2,
         title: 'python calculator',
-            description: 'An interactive escape room made with Javascript, PHP, HTML and CSS',
-                image: '/images/Calculator.jpg',
+            description: 'I created a Python calculator as a small utility tool that can perform quick arithmetic operations directly from the terminal. The calculator supports basic operations such as addition, subtraction, multiplication, division, and exponentiation, while also handling invalid input and division-by-zero errors gracefully. The program was written in Python, using a loop-based interface that allows users to continuously perform calculations until they choose to exit.',
+                image: '../public/images/Calculator.jpg',
         github: 'https://github.com/RafiqYousfi/Python-j1'
 }
 ];
